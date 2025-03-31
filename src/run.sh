b30b8bfbffcc0e8032f3f4e40b222d2c5feeaab4
watf-dev <wataru.fukuda.530@gmail.com>
2025-03-31 14:22:08 +0200

    fix: new watf

--- src/run.sh
+++ src/run.sh
@@ -7,7 +7,7 @@ ELE_Y=$2
 RANGE_X=10
 RANGE_Y=1
 
-BASE=$(pwd)
+BASE=$(readlink -f $(dirname $0))
 MESH=MESH
 MESH_GEN_REPO=https://github.com/watf-dev/MeshGeneration.git
 MESH_GEN_DIR=MeshGeneration/FEM
@@ -24,15 +24,15 @@ fi
 ### Generate mesh
 if [ ! -d $MESH ]; then
   ./$MESH_GEN_DIR/src/gen_mesh_FEM.py -o $MESH -e $ELE_X -e $ELE_Y -s 0 $RANGE_X -s 0 $RANGE_Y -p 1 -p 1 -n 2
-  gen_xdmf_wataf.py $MESH/mesh.cfg -o mesh.xmf2
+  gen_xdmf.py $MESH/mesh.cfg -o mesh.xmf2
 else
   echo "Mesh exists"
 fi
 
 ### Run FEM solver
-$BASE/src/FEM_solver.py $MESH/mesh.cfg --output_dir $OUTPUT --output_dis dis --output_disx disx --output_disy disy --output_nxx nxx --output_nyy nyy --output_nxy nxy --y_positive_flag
+$BASE/FEM_solver.py $MESH/mesh.cfg --output_dir $OUTPUT --output_dis dis --output_disx disx --output_disy disy --output_nxx nxx --output_nyy nyy --output_nxy nxy --y_positive_flag
 
 ### Generate XDMF files
-gen_xdmf_wataf.py $MESH/mesh.cfg -i $OUTPUT --fs dis n2f dis --fs disx n1f disx --fs disy n1f disy_abs -o dis.xmf2 
-gen_xdmf_wataf.py $MESH/mesh.cfg -i $OUTPUT --fs nxx n1f nxx --fs nyy n1f nyy --fs nxy n1f nxy -o stress.xmf2 
+gen_xdmf.py $MESH/mesh.cfg -i $OUTPUT --fs dis n2f dis --fs disx n1f disx --fs disy n1f disy_abs -o dis.xmf2 
+gen_xdmf.py $MESH/mesh.cfg -i $OUTPUT --fs nxx n1f nxx --fs nyy n1f nyy --fs nxy n1f nxy -o stress.xmf2 
 
--- src/watfFEM/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# Created: Jan, 28, 2025 12:00:47 by Wataru Fukuda
-
-from watfFEM.watfFEM import watfFEM
-
